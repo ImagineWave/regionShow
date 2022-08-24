062e5679a9d6cc751aee51a253171db24ff5cae8
Кукавский Владимир Витальевич <vkukavsky@blogic.ru>
2022-08-24 18:36:53 +0500

    Длина массива = 100 (0)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-document.write('4s4');
+let dataArray = Array();
+let placeArray = Array();
 load();
-function load(){
-    document.write();
-    fetch('http://localhost:63342/Prac4/data/small.csv').then((response) => {
+initPlaceArray();
+
+async function load(){
+    (fetch('http://localhost:63342/Prac4/data/small.csv').then((response) => {
         return response.text();
     }).then((data) => {
-        data.split(/ig/);
-        document.writeln(data);
-    })
+
+        let stringsData = data.split(/\n/);
+        for(let i = 0; i<stringsData.length; i++){
+            dataArray.push(stringsData[i]);
+            console.log(stringsData[i]);
+        }
+    }))
+
 }
+function initPlaceArray(){
+    console.log(dataArray); //TODO
+    console.log("dataArray.length = "+ dataArray.length); //TODO
+
+    for (let i = 0; i<dataArray.length; i++) {
+
+        let data = dataArray[i].match(/(\d+)";"(\d+)";"(\d+)";"(\d+)";"\d";"\d";"([А-Яа-я\s]+) ([А-Я а-я\s-]+)/gm);
+        placeArray.push(
+            {
+                code1: Number(data[1]),
+                code2: Number(data[2]),
+                code3: Number(data[3]),
+                code4: Number(data[4]),
+                status: String(data[5]),
+                name: String(data[6])
+
+            }
+        )
+    }
+}
+
 
